d2cc93d84bd9284d83f6ea923a4b45f7742a25d2
elenbokova <nansydrew@mail.ru>
2022-10-31 19:30:37 +0400

    Add footer with accordion

---
+++ source/js/modules/accordion-menu.js
@@ -0,0 +1,29 @@
+const footer = document.querySelector('[data-id="footer"]');
+const accordionButtons = document.querySelectorAll('[data-id="nav-toggle"]');
+
+const changeButtonState = (element) => {
+    if (element.getAttribute("data-state") === "is-closed") {
+        element.setAttribute("data-state", "is-opened");
+
+        accordionButtons.forEach((anotherElement) => {
+           if (anotherElement!==element)  {
+            anotherElement.setAttribute("data-state", "is-closed");
+           }
+        });
+    }
+    else { element.setAttribute("data-state", "is-closed"); }
+}
+
+const initAccordionMenu = () => {
+    footer.setAttribute("data-js-status", "is-active");
+    accordionButtons.forEach((el) => {
+        el.setAttribute("data-state", "is-closed");
+    });
+
+    accordionButtons.forEach((el) => {
+        el.addEventListener('click', () => changeButtonState(el));
+    });
+
+}
+
+export { initAccordionMenu };

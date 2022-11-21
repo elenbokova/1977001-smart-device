4665d0a8d96ed9a6c621e0f02d3472afaaabcc0d
elenbokova <nansydrew@mail.ru>
2022-11-21 13:41:58 +0400

    fix bug and delete phone checking

--- source/js/utils/phone-check.js
+++ source/js/utils/phone-check.js
@@ -15,30 +15,23 @@ const onPhoneInput = (evt) => {
     return input.value;
   }
 
-  if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
-    // Russian phone number
-    let firstSymbols = '+7';
-    formattedInputValue = firstSymbols + '';
+  let firstSymbols = '+7';
+  formattedInputValue = firstSymbols + '';
 
-    if (inputNumbersValue.length > 1) {
-      formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
-    }
+  if (inputNumbersValue.length > 1) {
+    formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+  }
 
-    if (inputNumbersValue.length >= 5) {
-      formattedInputValue += ')' + inputNumbersValue.substring(4, 7);
-    }
+  if (inputNumbersValue.length >= 5) {
+    formattedInputValue += ')' + inputNumbersValue.substring(4, 7);
+  }
 
-    if (inputNumbersValue.length >= 8) {
-      formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
-    }
+  if (inputNumbersValue.length >= 8) {
+    formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+  }
 
-    if (inputNumbersValue.length >= 10) {
-      formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
-    }
-
-  } else {
-    // Not Russian phone number
-    formattedInputValue = '+' + inputNumbersValue.substring(0, 11);
+  if (inputNumbersValue.length >= 10) {
+    formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
   }
 
   if (formattedInputValue.length < 11) {
